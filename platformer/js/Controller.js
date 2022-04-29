ab82febd0f46f4d5f68210b75a63de76ae5a4afa
pdlbq <pdbqlpdbq@gmail.com>
2022-04-29 19:01:32 +0300

    Update Controller.js

--- platformer/js/Controller.js
+++ platformer/js/Controller.js
@@ -7,6 +7,7 @@ class Controller {
 
 		this.leftClick = false;
 		this.positionClick;
+		this.positionTouch;
 
 		this.#addKeyboardListeners();
 	}
@@ -60,7 +61,7 @@ class Controller {
 
 		document.addEventListener('touchstart', (event) => {
 			this.leftClick = true;
-			this.positionClick = {x: event.touches[0].clientX, y: event.touches[0].clientY};
+			this.positionTouch = {x: event.touches[0].clientX, y: event.touches[0].clientY, x1: event.touches[1].clientX, y1: event.touches[1].clientY};
 		});
 
 		document.addEventListener('touchend', (event) => {
@@ -72,4 +73,8 @@ class Controller {
 	getPositionClick() {
 		return this.positionClick;
 	}
+	
+	getTouchClick() {
+		return this.positionTouch;
+	{
 }
